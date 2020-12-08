d8878aaaac3c96871156deec7a34deb298b58ec3
Erwin Amper <40266802+iuwin@users.noreply.github.com>
2020-12-08 16:48:20 +0800

    ui update and search navigation

--- lib/pages/home2.dart
+++ lib/pages/home2.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:sampleflutter/product.dart';
-import 'search_result.dart';
 import 'bag.dart';
 import 'profile.dart';
 
@@ -11,8 +10,9 @@ class Home2 extends StatefulWidget {
 
 class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
 
-  TabController _tabController;
   int _currentIndex = 0;
+  TabController _tabController;
+  final List<String> _tabs = ['Clothing', 'Gadgets', 'Furnitures', 'Toys', 'Hardware'];
   List<Product> products = new List<Product>();
   List<Product> clothing = new List<Product>();
   List<Product> gadgets = new List<Product>();
@@ -23,42 +23,21 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
 
   void _getProducts(){
     products = initProducts();
-    // products.forEach((product) => {
-    //   if(product.category == 'clothing'){
-    //     clothing.add(product)
-    //   }else if(product.category == 'gadgets'){
-    //     gadgets.add(product)
-    //   }else if(product.category == 'furniture'){
-    //     furnitures.add(product)
-    //   }else if(product.category == 'toy'){
-    //     toys.add(product)
-    //   }else if(product.category == 'hardware'){
-    //     hardware.add(product)  
-    //   }
-    // });
-    for (var product in products){
-      if(product != null){
-        print('wala man pota');
-        if(product.category == 'clothing'){
-          print('clothing');
-          clothing.add(product);
-          
-        }else if(product.category == 'gadgets'){
-          gadgets.add(product);
-          print('gadget');
-        }else if(product.category == 'furniture'){
-          furnitures.add(product);
-          print('furniture');
-        }else if(product.category == 'toy'){
-          toys.add(product);
-          print('toy');
-        }else if(product.category == 'hardware'){
-          hardware.add(product);
-          print('hardware');
-        }
+    products.forEach((product) => {
+      if(product.category == 'clothing'){
+        clothing.add(product)
+      }else if(product.category == 'gadgets'){
+        gadgets.add(product)
+      }else if(product.category == 'furniture'){
+        furnitures.add(product)
+      }else if(product.category == 'toy'){
+        toys.add(product)
+      }else if(product.category == 'hardware'){
+        hardware.add(product)  
       }
-    }
+    });
   } 
+
 
   @override
   void initState() {
@@ -67,9 +46,8 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
     _tabController = new TabController(length: 5, vsync: this);
   }
 
-
-  ///PRODUCT TEMPLATE
-  Widget _productTemplate(String imagepath, String productname, double price) {
+  ///product template
+  Widget _trendProductTemplate(String imagepath, String productname, double price) {
     return Center(
       child: Container(
         width: 150,
@@ -78,88 +56,138 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
           right: 20,
           bottom: 10,
         ),
-        padding: EdgeInsets.fromLTRB(00, 0, 0, 0),
         decoration: BoxDecoration(
-            color: Color(0xFFBF2F1F1),
+            color: Colors.white,//Color(0xFFBF2F1F1),
             borderRadius: BorderRadius.circular(30.0),
             boxShadow: [
               BoxShadow(
-                color: Colors.grey[200],
-                blurRadius: 10,
-                offset: Offset(1, 5),
+                color: Colors.grey[300],
+                blurRadius: 8,
+                offset: Offset(1, 3),
               )
-            ]),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Image.asset(imagepath, width: 120, height: 120,),
-            SizedBox(height: 10),
-            Container(
-              width: 145,
-              padding: EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5)
-              ),
-              child: Column(
-                children: [
-                  Text(productname, style: TextStyle(fontWeight: FontWeight.bold),),
-                  SizedBox(height: 5),
-                  Text('$price'),
-                ],
+            ]
+            ),
+        child: FittedBox(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset(imagepath, width: 120, height: 120,),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  children: [
+                    Text(productname, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                    SizedBox(height: 20),
+                    //Text('\$ $price', style: TextStyle(color: Color(0xFFB308278), fontWeight: FontWeight.bold, fontSize: 15)),
+                  ],
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
 
-  ///CATEGORIES
-  Widget _categoryTabBar() => TabBar(
+  ///trend categories
+  TabBar _trendTabBar() => TabBar(
     isScrollable: true,
     unselectedLabelColor: Colors.grey,
     labelColor: Colors.black,
     indicatorSize: TabBarIndicatorSize.label,
     indicatorColor: Color(0xFFB308278),
-    tabs: <Tab> [
-      Tab(
-        text: 'Clothing',
-      ),
-      Tab(
-        text: 'Gadgets'
-      ),
-      Tab(
-        text: 'Furnitures'
-      ),
-      Tab(
-        text: 'Toys'
-      ),
-      Tab(
-        text: 'Hardware'
-      )
-    ],
+    tabs: _tabs.map((tabName) => Tab(text: tabName,)).toList(),
     controller: _tabController,
   );
 
-  // Widget _categoryTabBarView(tabs) => TabBarView(
-  //   children: tabs,
-  //   controller: _tabController,
-  // );
 
-  Widget _categoryTabBarContent(List<Product> productList) => SingleChildScrollView(
+  SingleChildScrollView _trendTabBarContent(List<Product> productList) => SingleChildScrollView(
     scrollDirection: Axis.horizontal,
     padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
     child: Row(
       children: productList
-          .map((product) => _productTemplate(
-              product.imagePath, product.productName, 11000))
+          .map((product) => _trendProductTemplate(
+              product.imagePath, product.productName, 1000))
           .toList(),
     ),
   );
 
 
+  Widget _liveSellingTemplate() => Center(
+      child: Container(
+        width: 150,
+        height: 200,
+        margin: EdgeInsets.only(
+          right: 20,
+          bottom: 10,
+        ),
+        padding: EdgeInsets.only(top:15),
+        decoration: BoxDecoration(
+            color: Colors.white,//Color(0xFFBF2F1F1),
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[300],
+                blurRadius: 8,
+                offset: Offset(1, 3),
+              )
+            ]
+            ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  child: Center(child: Text('Live', style: TextStyle(color: Colors.white, fontSize: 13))),
+                ),
+                SizedBox(width: 7,),
+                Container(
+                  width: 70,
+                  height: 25,
+                  //padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.remove_red_eye, color: Colors.white, size: 15,),
+                      SizedBox(width: 3,),
+                      Text('246', style: TextStyle(color: Colors.white, fontSize: 13),)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          //   Image.asset(imagepath, width: 120, height: 120,),
+          //   SizedBox(height: 10),
+          //   Container(
+          //     padding: EdgeInsets.symmetric(vertical: 10),
+          //     child: Column(
+          //       children: [
+          //         // Text(productname, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+          //         // SizedBox(height: 20),
+          //         //Text('\$ $price', style: TextStyle(color: Color(0xFFB308278), fontWeight: FontWeight.bold, fontSize: 15)),
+          //       ],
+          //     )
+          //   )
+          ],
+        ),
+      ),
+    );
+
+  ///body
   Widget _homeBody(BuildContext context) {
     return CustomScrollView(
       slivers: [
@@ -168,12 +196,12 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
             automaticallyImplyLeading: false,
             backgroundColor: Colors.white,
             title: Icon(Icons.filter_list, color: Colors.black),
-            //title: Text('Home', style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold)),
             actions: [
               IconButton(
                 icon: Icon(Icons.search, color: Colors.black, size: 30,),
                 onPressed: (){
-                  showSearch(context: context, delegate: Search());
+                  Navigator.pushNamed(context, '/search');
+                  // showSearch(context: context, delegate: Search());
                 },
               ),
             ],
@@ -198,7 +226,7 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
               SizedBox(
                 height: 15,
               ),
-               _categoryTabBar(),
+               _trendTabBar(),
               SizedBox(
                 height: 25,
               ),
@@ -206,11 +234,11 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
                 height: 220,
                 child: TabBarView(
                   children: [
-                    _categoryTabBarContent(clothing),
-                    _categoryTabBarContent(gadgets),
-                    _categoryTabBarContent(furnitures),
-                    _categoryTabBarContent(toys),
-                    _categoryTabBarContent(hardware),
+                    _trendTabBarContent(clothing),
+                    _trendTabBarContent(gadgets),
+                    _trendTabBarContent(furnitures),
+                    _trendTabBarContent(toys),
+                    _trendTabBarContent(hardware),
                   ],
                   controller: _tabController,
                 ),
@@ -238,10 +266,17 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
                 scrollDirection: Axis.horizontal,
                 padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
                 child: Row(
-                  children: products
-                      .map((product) => _productTemplate(
-                          product.imagePath, product.productName, 11000))
-                      .toList(),
+                  children: [
+                    _liveSellingTemplate(),
+                    _liveSellingTemplate(),
+                    _liveSellingTemplate(),
+                    _liveSellingTemplate(),
+                    _liveSellingTemplate()  
+                  ],
+                  // children: products
+                  //     .map((product) => _trendProductTemplate(
+                  //         product.imagePath, product.productName, 11000))
+                  //     .toList(),
                 ),
               ),
             ]
@@ -264,7 +299,7 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
           mainAxisSpacing: 25.0,
           //childAspectRatio: 1.0,
           children: products
-              .map((product) => _productTemplate(
+              .map((product) => _trendProductTemplate(
                   product.imagePath, product.productName, 11000))
               .toList(),
         ),
@@ -273,8 +308,8 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
     );
   }
 
-
-  Widget _bottomNavBar() {
+  ///bottomnavbar
+  BottomNavigationBar _bottomNavBar() {
     return BottomNavigationBar(
         currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,
@@ -283,13 +318,11 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            title: Text(
-              'Home',
-            ),
+            title: Text('Home'),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.shopping_cart),
-            title: Text('Products'),
+            title: Text('Cart'),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
@@ -308,7 +341,7 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
       );
   }
 
-
+  ///build
   Widget build(BuildContext context) {
     final List<Widget> _children = [
       _homeBody(context),
@@ -326,68 +359,72 @@ class _Home2State extends State<Home2> with SingleTickerProviderStateMixin  {
 
 }
 
-//SEARCH FUNCTIONALITY
-class Search extends SearchDelegate<String>{
-
-  //example sa
-  List products = ['tshirt', 'bluetooth earphone', 'bluetooth speaker', 'fhm magazine', 'lisa poster', 'kids clothes', 'xiaomi phone'];
-  List recentSearched = ['earphones', 'twice merchs'];
-
-  // @override
-  // ThemeData appBarTheme(BuildContext context) {
-  //   assert(context != null);
-  //   final ThemeData theme = Theme.of(context);
-  //   assert(theme != null);
-  //   return theme;
-  // }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-      return [
-        IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: (){
-            query = '';
-          },
-        )
-      ];
-    }
-
-    @override
-    Widget buildLeading(BuildContext context) {
-      return IconButton(
-        icon: AnimatedIcon(
-          icon: AnimatedIcons.menu_arrow,
-          progress: transitionAnimation,
-        ),
-        onPressed: (){
-          this.close(context, null);
-        },
-      );
-    }
-
-    @override
-    Widget buildResults(BuildContext context) {
-      return SearchResult();
-    }
-
-    @override
-    Widget buildSuggestions(BuildContext context) {
-      final suggestionList = query.isEmpty
-          ? recentSearched
-          : products.where((p) => p.startsWith(query)).toList();
-
-      return ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-          onTap: (){
-            showResults(context);
-          },
-          title: Text(suggestionList[index]),
-        ),
-        itemCount: suggestionList.length,
-      );
-
-  }
 
 
-}
+///search
+// class Search extends SearchDelegate<String>{
+  // List products = ['tshirt', 'bluetooth earphone', 'bluetooth speaker', 'fhm magazine', 'lisa poster', 'kids clothes', 'xiaomi phone'];
+  // List recentSearched = ['earphones', 'twice merchs'];
+
+//   // @override
+//   // ThemeData appBarTheme(BuildContext context){
+//   //   final ThemeData theme = ThemeData(
+//   //     primaryColor: Color(0xFFB308278),
+//   //     primaryIconTheme: IconThemeData(color: Colors.white),
+//   //     primaryColorBrightness: Brightness.light,
+//   //     textTheme: TextTheme(title: TextStyle(color: Colors.white)),
+//   //     inputDecorationTheme: InputDecorationTheme(
+//   //       hintStyle: TextStyle(color: Color.fromARGB(90, 255, 255, 255))
+//   //     ),
+//   //     cursorColor: Colors.white
+//   //   );
+//   //   return theme;
+//   // }
+
+//   @override
+//   List<Widget> buildActions(BuildContext context) {
+//       return [
+//         IconButton(
+//           icon: Icon(Icons.clear),
+//           onPressed: (){
+//             query = '';
+//           },
+//         )
+//       ];
+//     }
+
+//   @override
+//   Widget buildLeading(BuildContext context) {
+//     return IconButton(
+//       icon: AnimatedIcon(
+//         icon: AnimatedIcons.menu_arrow,
+//         progress: transitionAnimation,
+//       ),
+//       onPressed: (){
+//         this.close(context, null);
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget buildResults(BuildContext context) {
+//     return SearchResult();
+//   }
+
+//   @override
+//   Widget buildSuggestions(BuildContext context) {
+//     final suggestionList = query.isEmpty
+//         ? recentSearched
+//         : products.where((p) => p.startsWith(query)).toList();
+
+//     return ListView.builder(
+//       itemBuilder: (context, index) => ListTile(
+//         onTap: (){
+//           showResults(context);
+//         },
+//         title: Text(suggestionList[index]),
+//       ),
+//       itemCount: suggestionList.length,
+//     );
+//   }
+// }
