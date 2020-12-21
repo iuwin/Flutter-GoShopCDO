85ae6c8c81509395b0a0ddaa7c40418ed28a6009
Erwin Amper <40266802+iuwin@users.noreply.github.com>
2020-12-21 15:31:24 +0800

    Add files via upload

--- lib/router.dart
+++ lib/router.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:sampleflutter/pages/search_result.dart';
+import 'package:sampleflutter/pages/search_screen.dart';
 import 'pages/loading.dart';
 import 'pages/login.dart';
 import 'pages/register.dart';
 import 'pages/home.dart';
-
+import 'pages/home2.dart';
+import 'pages/view_product.dart';
+import 'pages/bag.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
+
+  final args = settings.arguments;
 
   switch (settings.name) {
     case '/':
       return MaterialPageRoute(builder: (context) => LoadingScreen());
     case '/home':
-      return MaterialPageRoute(builder: (context) => Home());
+      return MaterialPageRoute(builder: (context) => Home2());
     case '/login':
       return MaterialPageRoute(builder: (context) => Login());
     case '/register':
       return MaterialPageRoute(builder: (context) => Register());
+    case '/viewproduct':
+      return MaterialPageRoute(builder: (context) => ProductView(productDetails: args));
+    case '/bag':
+      return MaterialPageRoute(builder: (context) => Bag());
+    case '/search':
+      return MaterialPageRoute(
+          builder: (context) =>
+              Search()); 
+    case '/searchresult':
+      return MaterialPageRoute(
+          builder: (context) =>
+              SearchResult()); 
     default:
       return MaterialPageRoute(builder: (context) => LoadingScreen());
   }
-
-
 }
