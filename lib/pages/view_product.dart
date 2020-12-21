37012a703b95fa8ea13efb4ecdc61f8d82542545
Erwin Amper <40266802+iuwin@users.noreply.github.com>
2020-12-21 15:31:04 +0800

    Add files via upload

--- lib/pages/view_product.dart
+++ lib/pages/view_product.dart
@@ -1,57 +1,57 @@
 import 'package:flutter/material.dart';
 
-
 class ProductView extends StatefulWidget {
+  Map productDetails = new Map();
+
+  ProductView({this.productDetails});
   @override
   _ProductViewState createState() => _ProductViewState();
 }
 
 class _ProductViewState extends State<ProductView> {
 
+
   ///Text Generator
-  Widget _textGenerator ({String text, Color color, double fontSize, FontWeight fontWeight}) => Container(
-    padding: EdgeInsets.fromLTRB(25, 15, 25, 0),
-    child:Text(
-      text, 
-      style: TextStyle(fontSize: fontSize, color: color, fontWeight: fontWeight),
-      textAlign: TextAlign.justify,
-    ),
-  );
-
-
+  Widget _textGenerator(
+          {String text, Color color, double fontSize, FontWeight fontWeight}) =>
+      Container(
+        padding: EdgeInsets.fromLTRB(25, 15, 25, 0),
+        child: Text(
+          text,
+          style: TextStyle(
+              fontSize: fontSize, color: color, fontWeight: fontWeight),
+          textAlign: TextAlign.justify,
+        ),
+      );
 
   ///Color Variation (Optional)
-  Widget _colorVariationGenerator ({Color color}) => CircleAvatar(
-    backgroundColor: color,
-    radius: 15,
-  );
-
-
+  Widget _colorVariationGenerator({Color color}) => CircleAvatar(
+        backgroundColor: color,
+        radius: 15,
+      );
 
   ///Upper Radius Effect (PNG only)
-  Widget _upperBorderRadius () => SliverToBoxAdapter(
-    child: Container(
-      color: Color(0xFFB308278),
-      height: 20,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-            height: 20,
-            decoration: BoxDecoration(
-              color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: const Radius.circular(40.0),
-                  topRight: const Radius.circular(40.0),
+  Widget _upperBorderRadius() => SliverToBoxAdapter(
+        child: Container(
+          color: Color(0xFFB308278),
+          height: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(40.0),
+                    topRight: const Radius.circular(40.0),
+                  ),
                 ),
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ),
-  );
-  
-
+        ),
+      );
 
   ///Build
   @override
@@ -64,9 +64,10 @@ class _ProductViewState extends State<ProductView> {
             backgroundColor: Color(0xFFB308278),
             expandedHeight: 200,
             flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset('assets/iphone12.png',
-              fit: BoxFit.contain,
-              ), 
+              background: Image.asset(
+                widget.productDetails['imagePath'], //PRODUCT IMAGE
+                fit: BoxFit.contain,
+              ),
             ),
           ),
           _upperBorderRadius(),
@@ -76,7 +77,11 @@ class _ProductViewState extends State<ProductView> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _textGenerator(text: 'iPhone 12', color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+                  _textGenerator(
+                      text: widget.productDetails['productName'], //PRODUCT NAME
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
                   Padding(
                     padding: EdgeInsets.only(right: 20),
                     child: Icon(Icons.favorite_border),
@@ -84,22 +89,39 @@ class _ProductViewState extends State<ProductView> {
                 ],
               ),
               //Price
-              _textGenerator(text: 'P5,000', color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              _textGenerator(
+                  text: 'P5,000',
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
               //Description
-              _textGenerator(text: 'Details', color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-              _textGenerator(text: 'The new iphone provides users the best technologies from apple. It is slick and have a variety of colors to choose from. Bill Gates said i love you.', color: Colors.grey[600], fontSize: 14, fontWeight: FontWeight.normal),
+              _textGenerator(
+                  text: 'Details',
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal),
+              _textGenerator(
+                  text:
+                      'The new iphone provides users the best technologies from apple. It is slick and have a variety of colors to choose from. Bill Gates said i love you.',
+                  color: Colors.grey[600],
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal),
               //Color Variation
-              _textGenerator(text: 'Colors', color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
+              _textGenerator(
+                  text: 'Colors',
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal),
               SizedBox(height: 10),
               Row(
                 children: [
-                  SizedBox(width:25),
+                  SizedBox(width: 25),
                   _colorVariationGenerator(color: Colors.blue),
-                  SizedBox(width:15),
+                  SizedBox(width: 15),
                   _colorVariationGenerator(color: Colors.black),
-                  SizedBox(width:15),
+                  SizedBox(width: 15),
                   _colorVariationGenerator(color: Colors.amber[600]),
-                  SizedBox(width:15),
+                  SizedBox(width: 15),
                   _colorVariationGenerator(color: Colors.grey),
                 ],
               ),
@@ -107,28 +129,32 @@ class _ProductViewState extends State<ProductView> {
           ),
         ],
       ),
-      bottomNavigationBar: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [ 
-          Container(
-            margin: EdgeInsets.only(bottom:10, top: 5),
-            padding: EdgeInsets.fromLTRB(75, 12, 75, 12),
-            child:Text('Buy now', textAlign: TextAlign.center, style: TextStyle(color:Colors.white, fontWeight: FontWeight.bold),),
-            decoration: BoxDecoration(color: Color(0xFFB296961), borderRadius: BorderRadius.circular(5)),
+      bottomNavigationBar:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 10, top: 5),
+          padding: EdgeInsets.fromLTRB(75, 12, 75, 12),
+          child: Text(
+            'Buy now',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          Container(
-            margin: EdgeInsets.only(bottom:10, top: 5),
-            padding: EdgeInsets.fromLTRB(30, 12, 30, 12),
-            child:Text('Add to Cart', textAlign: TextAlign.center, style: TextStyle(color:Colors.white, fontWeight: FontWeight.bold)),
-            decoration: BoxDecoration(color: Color(0xFFB296961), borderRadius: BorderRadius.circular(5)),
-          )
-        ]
-      ),
+          decoration: BoxDecoration(
+              color: Color(0xFFB296961),
+              borderRadius: BorderRadius.circular(5)),
+        ),
+        Container(
+          margin: EdgeInsets.only(bottom: 10, top: 5),
+          padding: EdgeInsets.fromLTRB(30, 12, 30, 12),
+          child: Text('Add to Cart',
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          decoration: BoxDecoration(
+              color: Color(0xFFB296961),
+              borderRadius: BorderRadius.circular(5)),
+        )
+      ]),
     );
   }
 }
-
-
-
-
-
